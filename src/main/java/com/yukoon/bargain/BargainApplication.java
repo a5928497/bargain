050f23e4deb1e8a5392ff0b3ebf47d8ab67bdaf9
a5928497 <61722615@qq.com>
2018-06-22 12:18:14 +0800

    ready

---
+++ src/main/java/com/yukoon/bargain/BargainApplication.java
@@ -0,0 +1,12 @@
+package com.yukoon.bargain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BargainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BargainApplication.class, args);
+	}
+}
