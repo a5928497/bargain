3bb3bcec2594f64414dee051157c05ac099610c2
a5928497 <61722615@qq.com>
2018-07-01 01:25:06 +0800

    developing

--- src/main/java/com/yukoon/bargain/controllers/GameController.java
+++ src/main/java/com/yukoon/bargain/controllers/GameController.java
@@ -7,14 +7,10 @@ import com.yukoon.bargain.services.GameService;
 import com.yukoon.bargain.services.RewardService;
 import com.yukoon.bargain.services.UserService;
 import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -29,6 +25,7 @@ public class GameController {
     private UserService userService;
     @Autowired
     private RewardService rewardService;
+
 
     /*
     * @return true
@@ -85,7 +82,11 @@ public class GameController {
             	//若用户未开记录，则新开记录并作第一次砍价后到活动详情
 				gameInfo.setUser(user);
 				gameInfo.setReward(rewardService.findById(gameInfo.getReward().getId()));
+				//新开记录
 				gameService.newRecord(gameInfo);
+				//第一次砍价
+                gi_temp = gameService.findByActIdAndUserId(gameInfo.getActivity().getId(),user.getId());
+                gameService.bargain(gi_temp);
 			}else {
 				//若用户已开记录，则直接到活动详情
 			}
