3cca682c1586b4fa66a20bbcfe87c4aba9d43778
a5928497 <61722615@qq.com>
2018-07-04 00:14:29 +0800

    basic complete

--- src/main/java/com/yukoon/bargain/utils/PageableUtil.java
+++ src/main/java/com/yukoon/bargain/utils/PageableUtil.java
@@ -1,5 +1,6 @@
 package com.yukoon.bargain.utils;
 
+import com.sun.tools.javadoc.Start;
 import com.yukoon.bargain.entities.Page;
 import com.yukoon.bargain.entities.User;
 
@@ -28,12 +29,17 @@ public class PageableUtil {
     public static Page page(Integer pageNo,Integer pageSize,List list) {
         Page page  = new Page();
         //总页数
-        int total = (list.size()%pageSize) ==0?(list.size()/pageSize):((list.size()/pageSize)+1);
+        int total = 1;
+        int start = 0;
+        int end = list.size()-1;
+        if (list.size()>=pageSize) {
+            total = (list.size()%pageSize) ==0?(list.size()/pageSize):((list.size()/pageSize)+1);
+            //元素下标起点
+            start = (pageNo -1)*pageSize;
+            end = (pageNo * pageSize) -1;
+        }
         page.setPageTotal(total);
         page.setPageNo(pageNo).setPageSize(pageSize);
-        //元素下标起点
-        int start = (pageNo -1)*pageSize;
-        int end = (pageNo * pageSize) -1;
         ArrayList list_new = new ArrayList();
         for (int i = start;i<= end ;i++) {
             list_new.add(list.get(i));
@@ -42,4 +48,7 @@ public class PageableUtil {
         return page;
     }
 
+    public static void main(String[] args) {
+        System.out.println(2%10);
+    }
 }
