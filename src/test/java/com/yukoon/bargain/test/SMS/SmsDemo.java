406e42d5d11125cbfc7b23065a4af22de3aea02e
a5928497 <61722615@qq.com>
2018-07-06 00:42:56 +0800

    developing

--- src/test/java/com/yukoon/bargain/test/SMS/SmsDemo.java
+++ src/test/java/com/yukoon/bargain/test/SMS/SmsDemo.java
@@ -43,8 +43,8 @@ public class SmsDemo {
     public static SendSmsResponse sendSms() throws ClientException {
 
         //可自助调整超时时间
-        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
-        System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+        System.setProperty("sun.net.client.defaultConnectTimeout", "100000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "100000");
 
         //初始化acsClient,暂不支持region化
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
@@ -58,9 +58,9 @@ public class SmsDemo {
         //必填:短信签名-可在短信控制台中找到
         request.setSignName("宇坤信息");
         //必填:短信模板-可在短信控制台中找到
-        request.setTemplateCode("验证码");
+        request.setTemplateCode("SMS_139215013");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
-        request.setTemplateParam("{\"code\":\"123\"}");
+        request.setTemplateParam("{\'code\':\'1233\'}");
 
         //选填-上行短信扩展码(无特殊需求用户请忽略此字段)
         //request.setSmsUpExtendCode("90997");
