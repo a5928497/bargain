8ba873c576616eb2f58662c5c7baee8c6ec507b6
a5928497 <61722615@qq.com>
2018-07-09 20:47:23 +0800

    developing

--- src/main/java/com/yukoon/bargain/controllers/UploadController.java
+++ src/main/java/com/yukoon/bargain/controllers/UploadController.java
@@ -5,9 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -28,5 +34,33 @@ public class UploadController {
         }
         map.put("act_id",act_id);
         return "backend/code_excel_upload";
+    }
+
+    //后台进行兑换码Excel上传
+    @PostMapping("/uploadcode")
+    public String uploadExcel(@RequestParam("excel")MultipartFile excel, Integer act_id, RedirectAttributes attributes) {
+        List<String> repeatNames;
+        try {
+            InputStream in = excel.getInputStream();
+            repeatNames = uploadService.importCodeExcel(in,excel,act_id);
+            in.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("失败");
+            attributes.addFlashAttribute("uploadMsg","上传失败，请重试！");
+            return "redirect:/touploadcode/"+act_id;
+        }
+        System.out.println("成功");
+        if (repeatNames.size() != 0 ){
+            String uploadMsg ="上传成功！并去除了EXCEL表中重复的:\n";
+            for (String s :repeatNames) {
+                uploadMsg = uploadMsg + s + ",\n";
+            }
+            uploadMsg = uploadMsg + "一共"+ repeatNames.size() + "个兑换码";
+            attributes.addFlashAttribute("uploadMsg",uploadMsg);
+        }else {
+            attributes.addFlashAttribute("uploadMsg","上传成功！");
+        }
+        return "redirect:/touploadcode/"+act_id;
     }
 }
