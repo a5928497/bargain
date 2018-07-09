c501432122302fc7569655f37c7e7ed6a62cf6b6
a5928497 <61722615@qq.com>
2018-07-09 23:57:15 +0800

    developing

--- src/main/java/com/yukoon/bargain/services/RedeemCodeService.java
+++ src/main/java/com/yukoon/bargain/services/RedeemCodeService.java
@@ -57,7 +57,9 @@ public class RedeemCodeService {
         return PageableUtil.page(pageNo,pageSize,list_new);
     }
 
-    //验证某一活动下所有礼品对应的兑换码是否足够发放给所有中奖的客户，若不足够，返回不足礼品的id和数量
+    //验证某一活动下所有礼品对应的兑换码是否足够发放给所有中奖的客户
+    // 若不足够，返回不足礼品的id和数量
+    // 若足够，进行批量兑换
     @Transactional
     public Map<Integer,Integer> batchCashingCheck(Integer act_id) {
         boolean flag = false;
@@ -66,13 +68,13 @@ public class RedeemCodeService {
         for (Reward reward : rewards) {
             //获得该礼品对应的未被使用的兑换码数量
             List<RedeemCode> list = redeemCodeRepo.findCodeByRewardId(reward.getId());
-            List<RedeemCode> list_new  = new ArrayList<>();
+            List<RedeemCode> codes_new  = new ArrayList<>();
             for (RedeemCode rc:list) {
                 if (rc.getWinner() == null) {
-                    list_new.add(rc);
+                    codes_new.add(rc);
                 }
             }
-            Integer code_size = list_new.size();
+            Integer code_size = codes_new.size();
             //获得该礼品对应的中奖者数量
             List<GameInfo> winners = gameInfoRepo.whoswin(act_id);
             List<GameInfo> winners_new = new ArrayList<>();
@@ -83,11 +85,22 @@ public class RedeemCodeService {
             }
             Integer winners_size = winners_new.size();
             flag = code_size >= winners_size;
+            //若兑换券小于中奖用户，不进行兑换，加入返回信息
             if (!flag) {
                 map.put(reward.getId(),winners_size-code_size);
+            }else {
+                //若兑换券大于等于中奖用户，足够兑换，则进行兑换
+                for (int i = 0;i<winners_size;i++) {
+                    GameInfo gi_temp = winners_new.get(i);
+                    RedeemCode rc_temp = codes_new.get(i);
+                    gi_temp.setRedeemCode(rc_temp);
+                    rc_temp.setWinner(gi_temp.getUser()).setAwardDate(new Date());
+                }
+                gameInfoRepo.save(winners_new);
+                redeemCodeRepo.save(codes_new);
             }
         }
-        return map;
+            return map;
     }
 
     @Transactional
