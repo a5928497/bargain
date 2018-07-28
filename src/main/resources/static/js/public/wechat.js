a1136542059b058123701ec62d09691b6a0be934
a5928497 <61722615@qq.com>
2018-07-28 19:20:10 +0800

    develpoing

--- src/main/resources/static/js/public/wechat.js
+++ src/main/resources/static/js/public/wechat.js
@@ -42,7 +42,7 @@ $(function () {
         wx.ready(function () {
             // 微信分享的数据
             var shareData = {
-                "imgUrl" : localhostPaht+"/thanks.png",    // 分享显示的缩略图地址
+                "imgUrl" : localhostPaht+"/images/thanks.png",    // 分享显示的缩略图地址
                 "link" : url,    // 分享地址
                 "desc" : desc,   // 分享描述
                 "title" : title,   // 分享标题
