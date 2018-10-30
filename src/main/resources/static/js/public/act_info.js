79e7c552e99f3292eb8d672fb891d8011e1c9f4a
a5928497 <61722615@qq.com>
2018-10-30 22:47:26 +0800

    fixing

--- src/main/resources/static/js/public/act_info.js
+++ src/main/resources/static/js/public/act_info.js
@@ -7,6 +7,10 @@ $(function () {
     var pathName=window.document.location.pathname;
     //前缀
     var localhostPaht=curWwwPath.substring(0,curWwwPath.indexOf(pathName));
+    $wrapper = $("#main_wrapper");
+    //初始化图片元素
+    $wrapper.css("background-image","url("+ localhostPaht +"/basic_images/act_info_pg/actinfo_background.jpg)");
+    $joinBTN.css("background-image","url("+ localhostPaht +"/basic_images/act_info_pg/join_btn.png)");
     $("#join_back_url").val(pathName);
     //添加参与活动按钮监听
     $joinBTN.click(function () {
