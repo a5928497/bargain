2040aecc4dc5080e3cdf2ed06afee8d347ff0141
a5928497 <61722615@qq.com>
2018-10-31 11:38:28 +0800

    develpoing

--- src/main/resources/static/js/public/user_info.js
+++ src/main/resources/static/js/public/user_info.js
@@ -2,6 +2,16 @@ $(function () {
     $codeInfoBTN = $(".codeInfoBTN");
     $addressForm = $("#addressForm");
     $addressForm.hide();
+    //完整路径
+    var curWwwPath=window.document.location.href;
+    //后缀
+    var pathName=window.document.location.pathname;
+    //前缀
+    var localhostPaht=curWwwPath.substring(0,curWwwPath.indexOf(pathName));
+    $wrapper = $("#main_wrapper");
+    //初始化图片元素
+    $wrapper.css("background-image","url("+ localhostPaht +"/basic_images/user_info_pg/userinfo_background.jpg)");
+
     $codeInfoBTN.click(function () {
         $this = $(this);
         var gameinfo = $this.attr("gameinfo");
