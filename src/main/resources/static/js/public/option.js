99e85035540ea1fb70c6d589d9b7a5dd655df27a
a5928497 <61722615@qq.com>
2018-11-01 18:57:50 +0800

    develpoing

--- src/main/resources/static/js/public/option.js
+++ src/main/resources/static/js/public/option.js
@@ -10,11 +10,24 @@ $(function () {
     $arrow_right_container  = $("#right_arrow_container");
     $backBTN = $("#backBTN");
     $reward_left = $("#reward_left");
+    $wrapper = $("#main_wrapper");
+    $submitBTN = $("#submitBTN");
     var size = $rewards.size();
     var location = 0;
     var body_container_height = $("#body_container").height();
     var image_size = body_container_height * 0.66;
+    //完整路径
+    var curWwwPath=window.document.location.href;
+    //后缀
+    var pathName=window.document.location.pathname;
+    //前缀
+    var localhostPaht=curWwwPath.substring(0,curWwwPath.indexOf(pathName));
     //初始化图片
+    $wrapper.css("background-image","url("+ localhostPaht +"/basic_images/option_pg/option_background.jpg)");
+    $arrow_left.css("background-image","url("+ localhostPaht +"/basic_images/option_pg/arrows_left.png)");
+    $arrow_right.css("background-image","url("+ localhostPaht +"/basic_images/option_pg/arrows_right.png)");
+    $backBTN.css("background-image","url("+ localhostPaht +"/basic_images/option_pg/back_btn.png)");
+    $submitBTN.css("background-image","url("+ localhostPaht +"/basic_images/option_pg/select_btn.png)");
     $reward_img.attr("src","/reward_images/reward"+$(".rewards:eq(0)").val() +".png");
     $reward_name.text($(".rewards:eq(0)").attr("name"));
     $reward_price.text($(".rewards:eq(0)").attr("price")+"刀");
